308a604b193e3b42c72994bcb0043b83d679da7d
Paulo Correa <pauloyaco@gmail.com>
2020-06-02 01:51:30 -0300

    Correcao de remocao de docker machine

--- deploy.prod.sh
+++ deploy.prod.sh
@@ -3,6 +3,8 @@ echo "--------------------------------------------------------\n"
 echo "-------        SCRIPT DE DEPLOY NA AWS         ---------\n"
 echo "--------------------------------------------------------\n"
 echo "Iniciando deploy....\n"
+echo "Removendo instancia existente.."
+docker-machine rm docker-node -y
 echo "Criando maquina docker na aws....\n"
 docker-machine create --driver amazonec2 --amazonec2-open-port 3000 docker-node
 echo "Habilitando o ambiente virtual....\n"
@@ -12,6 +14,7 @@ eval $(docker-machine env docker-node)
 echo "Subindo container docker....\n"
 make up
 echo "Finalizando deploy na AWS....\n"
+aws ec2 describe-instances --query "Reservations[*].Instances[*].{KeyName='docker-node'}"
 echo "Script finalizado...\n"
 echo "--------------------------------------------------------\n"
 echo "-------    FIM DE SCRIPT DE DEPLOY NA AWS      ---------\n"
